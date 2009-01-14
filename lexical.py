fdbb63801bacd5ed0f2f267bbf5ab6830962d401
Stanislav M. Ivankin <stas@concat.info>
2009-01-15 07:52:39 +0800

    * lexical.py (l_print): print out in lambda (normalize_list): Grow one of lists to the size of another if needed (find_min._find_min): Find minimal distance in list of words for entity (total_phrase_diff): name changed (PhraseCheck): Generates sorted phrase

--- lexical.py
+++ lexical.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re
+import re, sys
+
+l_print = lambda *x:sys.stdout.write(" ".join(map(str,x)) + '\n')
 
 def cycle_list (lst, dir=1):
     if dir == 1:
@@ -9,12 +11,31 @@ def cycle_list (lst, dir=1):
     else:
         return lst[1:] + [lst[0]]
 
+def normalize_list (lst1, lst2):
+    if len(lst1) < len(lst2):
+        lst1 = lst1 + ['']*(len(lst2) - len(lst1))
+    elif len(lst2) < len(lst1):
+        lst2 = lst2 + ['']*(len(lst1) - len(lst2))
+
+    return lst1, lst2
+
+def find_min (entity, lst):
+    score = lev_distance (entity, lst[0])
+
+    def _find_min (entity, lst, score):
+        if lst:
+            score_new = lev_distance (entity, lst[0])
+            return _find_min (entity, lst[1:], (score_new < score and [score_new] or [score])[0])
+        else:
+            return score
+
+    return _find_min (entity, lst[1:], score)
+
 # TODO: Don't forget to handle upper case
 def lev_distance(word1, word2):
     """Find distance between words, used to guess errors between
     tags of the same entity
-    """
-    
+    """    
     n, m = len(word1), len(word2)
     if n > m:
         word1, word2 = word2, word1
@@ -30,42 +51,44 @@ def lev_distance(word1, word2):
             
     return current_row[n]
 
-
-# TODO: This is dummy function, the idea is:
-# Try to find errored written words, Then try to swap words
-# Концерты Визбора | Визбор - Концерты
-def PhraseCheck(phrase1, phrase2):
+def total_phrase_diff (phrase1, phrase2):
     match = re.compile('[^\w]+', re.UNICODE)
-    lst1, lst2 = match.split(phrase1), match.split(phrase2)
-    if len(lst1) < len(lst2):
-        lst1 = lst1 + ['']*(len(lst2) - len(lst1))
-    elif len(lst2) < len(lst1):
-        lst2 = lst2 + ['']*(len(lst1) - len(lst2))
+    lst1, lst2 = normalize_list (match.split(phrase1), match.split(phrase2))
+
     res = map(lambda x,y: lev_distance(x,y), lst1, lst2)
-    print res
+    
     return reduce(lambda x,y: x + y, res)
 
-# Idiotizm:
-def PhraseCheck(phrase1, phrase2):
+def PhraseCheck (phr1, phr2):
     match = re.compile('[^\w]+', re.UNICODE)
-    lst1, lst2 = match.split(phrase1), match.split(phrase2)
+    lst1, lst2 = normalize_list (match.split(phr1), match.split(phr2))
+    lng = len(lst1)
+    res_lst = [''] * lng
+
+    def _dist_sort (x, y):
+        if x[1]>y[1]:
+            return 1
+        elif x[1]==y[1]:
+            return 0
+        else: 
+            return -1
     
-    if len(lst1) < len(lst2):
-        lst1 = lst1 + ['']*(len(lst2) - len(lst1))
-    elif len(lst2) < len(lst1):
-        lst2 = lst2 + ['']*(len(lst1) - len(lst2))
+    index = 0
+    dist = []
+    for elem1 in lst1:
+        dist_inner = []
+        for i, elem2 in enumerate (lst2):
+            dist_inner += [[i, lev_distance (elem1, elem2)]]
+        dist += [dist_inner]
 
-    res = reduce(lambda x,y: x + y,
-                 map(lambda x,y: lev_distance(x,y), lst1, lst2))
-    for x in xrange(len(lst1)):
-         lst_tmp = cycle_list(lst1)
-         res_tmp = reduce(lambda x,y: x + y,
-                          map(lambda x,y: lev_distance(x,y), lst_tmp, lst2))
-         if (res_tmp < res):
-             lst1 = lst_tmp
-    return lst1
-         
-        
+    print dist
+    for row in dist:
+        row.sort(cmp=_dist_sort)
+        print row
+#        print reduce (lambda x,y: (x[1] < y[1] and [x] or [y])[0], row)
+            
 
-res = PhraseCheck(u'Концерты - тут Визбор былв', u'Концерты Визбора были тут')
-print u' '.join(res)
+print PhraseCheck("abc cba bbb", "cbdsadsaaa ac bbbbbbbbb")
+
+#res = PhraseCheck(u'Труляля гулял покопчёному морю', u'пакопчёному Труляля гулял морю')
+
